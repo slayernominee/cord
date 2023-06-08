fc39c325d980b195231ce643a84b12f548bba2fa
slayernominee <slayernominee_git@proton.me>
2023-06-09 00:20:02 +0200

    @me route instead of app

--- components/ServerList.vue
+++ components/ServerList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-slate-950 w-20 h-full text-center">
-        <div class="servericon" @click="$router.push('/app')">
+        <div class="servericon" @click="$router.push('/channel/@me')">
             <img class="rounded-full w-[70%]" src="https://cdn.discordapp.com/icons/1113941024302178416/f7371b2242a268657b388d7136718fc8.webp?size=240">
         </div>
         
